059bf10903e0c979d87230f425e3702220131c58
veni75 <vardai.eniko@gmail.com>
2021-07-05 15:14:46 +0200

    task3

--- task-03/index.js
+++ task-03/index.js
@@ -2,13 +2,13 @@
  * 1. Töltsd be a json állományok kezelését végző modult az azonos mappából és 
  * emeld ki egy változóba az update metódusát.
  */
-
+ const {update} = require('./jsonHandler');
 /**
  * 2. Ebben a névtelen aszinkron függvényben futtatsd az update metódust. 
  * Ha készen van a json kezelő modulod, akkor csupán élesítened kell az alábbi 
  * kódot a komment jelek eltávolításával.
  */
-/*
+
 ( async () => {
     console.log( await update({
         "id": 7,
@@ -17,4 +17,4 @@
         "bestBefore": "2021-05-30"
     }));
 })();
-*/
+
--- task-03/jsonHandler.js
+++ task-03/jsonHandler.js
@@ -1,20 +1,23 @@
 /**
  * 1. A fájlok kezeléséhez az fs modul promise alapú verzióját használd.
  */
-
+ const {readFile,writeFile} = require('fs').promises
 /**
  * 2. Állítsd be az azonos mappában található .json fájl elérési útját a path 
  * modul join metódusának segítségével.
  */
- const jsonPath = '';
+ 
+ const { join } = require('path');
+ const jsonPath = join(__dirname, 'db', 'products.json');
 
  /**
   * 3. A jsonPath útvonalon található fájl tartalmát beolvassa és értelmezi, 
   * majd visszaadja a kapott tömböt.
   * @returns objektumok tömbje
   */
- const getList = async () => {
-     //
+ const getList = async (file) => {
+    const list = await readFile(file)
+    return JSON.parse(list)
  };
 
 /**
@@ -23,8 +26,9 @@
  * @param {Array} list objektumok tömbje
  * @returns 
  */
-const saveList = async (list = []) => {
-    //
+const saveList = async (file, list = []) => {
+    const newlist = await writeFile(file, JSON.stringify(list))
+    return newlist
 };
 
 /**
@@ -35,7 +39,11 @@ const saveList = async (list = []) => {
  * @returns a frissített objektum ha sikerült a frissítés, egyébként false
  */
 const update = async (entity = {}) => {
-    //
+    const list = await getList(jsonPath);
+    const index = list.findIndex( item => item.id === entity.id );
+    list[index] = {...list[index], ...entity};    
+    await saveList(jsonPath, list);    
+    return list[index];
 };
 
 module.exports = {
